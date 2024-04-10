ec49d6e4daee9fbda4f6bc45cf2736c7a2161c4c
Ryguy0000 <154478432+Ryguy0000@users.noreply.github.com>
2024-04-10 07:35:29 -0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,33 @@
-let Screen_spot = 0
-basic.forever(function () {
-	
-})
+let list = []
+namespace Big_Maps {
+    //% block=Array1
+    export function Array1(): number[] {
+        return list = [
+            1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1
+        ];
+    }
+    //% block=Array2
+    export function Array2(): number[] {
+        return list = [
+            1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1
+        ];
+    }
+    //% block=Array3
+    export function Array3(): number[] {
+        return list = [
+            1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1
+        ];
+    }
+    //% block=Array4
+    export function Array4(): number[] {
+        return list = [
+            1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1
+        ];
+    }
+    //% block=Array5
+    export function Array5(): number[] {
+        return list = [
+            1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1
+        ];
+    }
+}
